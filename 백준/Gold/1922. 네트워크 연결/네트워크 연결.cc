41bf79f7dedb004ec82958f6aadd52467b82ff55
thekim123 <tacdo12@naver.com>
2025-12-14 17:03:41 +0900

    [Gold IV] Title: 네트워크 연결, Time: 92 ms, Memory: 3620 KB -BaekjoonHub

---
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.cc
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+struct Edge {
+	int u, v;
+	long weight;
+
+	bool operator < (const Edge& o) const {
+		return weight < o.weight;
+	}
+};
+
+struct UnionFind {
+	vector<int> group;
+	int num_groups;
+
+	UnionFind(int num_groups) {
+		this->group.resize(num_groups);
+		this->num_groups = num_groups;
+		for (int i = 0; i < num_groups; i++) {
+			group[i] = i;
+		}
+	}
+
+	int Find(int p) {
+		if (group[p] == p) return p;		
+		group[p] = Find(group[p]);
+		return group[p];
+	}
+
+	bool Connected(int p, int q) {
+		return Find(p) == Find(q);
+	}
+
+	void Union(int p, int q) {
+		int pid = Find(p);
+		int qid = Find(q);
+
+		if (pid == qid) return;
+		group[pid] = qid;
+		num_groups--;
+	}
+};
+
+int main() {
+	int N, M;
+	cin >> N >> M;
+
+	int a, b;
+	long c;
+	vector<Edge> edges(M);
+	for (int i = 0; i < M; i++) {
+		cin >> a >> b >> c;
+		Edge e = { a-1,b-1,c };
+		edges[i] = e;
+	}
+	sort(edges.begin(), edges.end());
+
+	UnionFind uf = UnionFind(N);
+	int result = 0;
+	for (int i = 0; i < M; i++) {
+		Edge& e = edges[i];
+		if (!uf.Connected(e.u, e.v)) {
+			uf.Union(e.u, e.v);
+			result += e.weight;
+		}
+	}
+
+	cout << result;
+}
